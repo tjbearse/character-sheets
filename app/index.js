cf92e37ad8a8083c880e7dccf35c01a2ebd64c3b
TJ Bearse <bearse.thomas@gmail.com>
2021-03-29 00:09:41 -0700

    add redux toolkit quick start
    https://redux-toolkit.js.org/tutorials/quick-start

--- app/index.js
+++ app/index.js
@@ -1,16 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import store from './app/store'
+import { Provider } from 'react-redux'
 
-import MyImage from './assets/dog.jpg';
-import './index.css';
-
-const App = () => {
-	return (
-		<div>
-			<div>Welcome to my-webpack-react-starter</div>
-			<img src={MyImage} />
-		</div>
-	);
-};
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
